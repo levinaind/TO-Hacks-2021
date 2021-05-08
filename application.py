148b2d9c4f626b8ddb1e6b7aac653047e3cb5a71
Nicholaus Suprapto <54722313+ininicho@users.noreply.github.com>
2021-05-08 12:10:30 -0400

    Added app.py

---
+++ application.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask, session
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+database = scoped_session(sessionmaker(bind=engine))
+db = database()
+
+# INDEX
+@app.route("/", methods=["GET","POST"])
+def index():
+    return "Hello, World!"
